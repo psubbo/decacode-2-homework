9ba21892f71e0150c26ad5cde74fd9abc2fa0d24
psubbo <psubbo@yandex.ru>
2018-09-13 21:52:43 +0300

    hw3_start

---
+++ 3-functions/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathToJSON = path.resolve(__dirname, 'index.json');
+const data = fs.readFileSync(pathToJSON, 'utf8');
+
+// your code...
+// console.log(count);
+// console.log(averageAge);
