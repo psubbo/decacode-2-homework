bdbffe65a38f0919dd0a22d62ff5f3a3ad6b8fad
psubbo <psubbo@yandex.ru>
2018-09-13 23:43:40 +0300

    done

--- 3-functions/index.js
+++ 3-functions/index.js
@@ -4,37 +4,20 @@ const fs = require('fs');
 const pathToJSON = path.resolve(__dirname, 'index.json');
 const data = fs.readFileSync(pathToJSON, 'utf8');
 
-let users = [{
-    "name": "Domenico Alfani",
-    "age": 35,
-    "skills": ["Paint", "Theatre"]
-  }, {
-    "name": "Pietro Annigoni",
-    "age": 19,
-    "skills": ["Music"]
-  }, {
-    "name": "Jacopo Bassano",
-    "age": 24,
-    "skills": ["Paint", "Philisophy"]
-  }, {
-    "name": "Giovanni di Paolo",
-    "age": 47,
-    "skills": ["Paint", "Music"]
-  }, {
-    "name": "Francesco Hayez",
-    "age": 20,
-    "skills": ["Music", "Theatre"]
-  }, {
-    "name": "Ulvi Liegi",
-    "age": 30,
-    "skills": ["Paint"]
-  }]
+let users = JSON.parse(data);
 
-//let users = JSON.parse(data);
-
+function findPainters(p){
+    let painters = new Array();
+    p.forEach(function(item, i, arr) {
+        if ( p[i].skills.indexOf("Paint") != -1 ){
+            painters.push(p[i].name); 
+        };
+      });
+return painters;
+}
 
 function countAverageAge (p){
-   let sumAge = 0;
+   let sumAge = new Number();
     p.forEach(function(item, i, arr) {
         sumAge = sumAge + Number(p[i].age);
 
@@ -44,17 +27,14 @@ function countAverageAge (p){
 }
 
 function createList (p){
-    let stringUsers = "";
+    let stringUsers = new String();
     p.forEach(function(item, i, arr) {
         stringUsers = stringUsers + p[i].age + " " + p[i].name + ", ";
       });
 return stringUsers.slice(0, -2);
-}
+};
 
 console.log('Количество пользователей: ' + users.length);
 console.log('Средний возраст пользователей: ' + countAverageAge(users));
-console.log(createList(users));
-
-// your code...
-// console.log(count);
-// console.log(averageAge);
+console.log('Все пользователи: ' + createList(users));
+console.log('Художники:  - ' + findPainters(users));
